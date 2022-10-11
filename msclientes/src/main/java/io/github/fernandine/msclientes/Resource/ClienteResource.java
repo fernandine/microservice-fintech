3f70f1e6ae81fd82e93961379da5417de294d0de
Jean Fernandine <jfernandine@gmail.com>
2022-10-11 14:45:36 -0300

    cloud gateway

---
+++ msclientes/src/main/java/io/github/fernandine/msclientes/Resource/ClienteResource.java
@@ -0,0 +1,50 @@
+package io.github.fernandine.msclientes.Resource;
+
+import io.github.fernandine.msclientes.Resource.representation.ClienteSaveRequest;
+import io.github.fernandine.msclientes.services.ClienteService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+
+@RestController
+@RequestMapping("clientes")
+@RequiredArgsConstructor
+@Slf4j
+public class ClienteResource {
+
+    private final ClienteService service;
+
+    @GetMapping
+    public String status(){
+        log.info("Obtendo o status do microservice de clientes");
+        return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request) {
+        var cliente = request.toModel();
+        service.save(cliente);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente(@RequestParam("cpf") String cpf) {
+        var cliente = service.getByCpf(cpf);
+        if(cliente.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
+    }
+
+}
